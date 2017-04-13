38c0fa1e34a236004a0e175a14b15c9f42cd6b34
jlanej <jlanej@gmail.com>
2017-04-12 21:47:09 -0500

    vcf compute that does nothing

--- tance.go
+++ tance.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/urfave/cli"
 	"os"
 )
 
+func wopAction(c *cli.Context) error {
+	fmt.Fprintf(c.App.Writer, ":wave: over here, eh\n")
+	return nil
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "tance"
-	app.Usage = "Generate T-SNE plot from genotypes to visualize ancestry"
+	app.Usage = "Generates t-Distributed Stochastic Neighbor Embedding (t-SNE) from genotype data"
 	app.Version = "v0.0.1"
 	var threads int
+	var vcf string
 
-	app.Flags = []cli.Flag {
+	app.Flags = []cli.Flag{
 		cli.IntFlag{
 			Name:        "threads, t",
 			Usage:       "number of threads to use `INT` ",
@@ -22,24 +29,33 @@ func main() {
 	}
 	app.Commands = []cli.Command{
 		cli.Command{
-			Name:        "tsne",
-			Aliases:     []string{"t"},
-			Category:    "tsne",
-			Usage:       "generate tsne from genotype data",
-			UsageText:   "tsne - generates tsne from genotype data",
-			Description: "sss",
-			ArgsUsage:   "[arrgh]",
+			Name:    "vcf-compute",
+			Aliases: []string{"vc"},
+			//Category: "compute",
+			Usage:       "compute t-SNE from vcf input ",
+			Description: "compute (t-SNE) from genotypes in vcf file",
 			Flags: []cli.Flag{
-				cli.BoolFlag{Name: "forever, forevvarr"},
-			},
-			Subcommands: cli.Commands{
-				cli.Command{
-					Name:   "wop",
-					//Action: wopAction,
+				cli.StringFlag{
+					Name:        "vcf, f",
+					Usage:       "vcf `FILE` to use ",
+					Destination: &vcf,
 				},
+			},
+			Action: func(c *cli.Context) error {
+				name := "Nefertiti"
+				if c.NArg() > 0 {
+					name = c.Args().Get(0)
+				}
+				if c.String("lang") == "spanish" {
+					fmt.Println("Hola", name)
+				} else {
+					fmt.Println("Hello", vcf)
+				}
+				return nil
 			},
 		},
 	}
 
 	app.Run(os.Args)
+
 }
